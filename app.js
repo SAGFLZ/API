b31613b74d2f52dc45f0728f825e2a4765363ae7
Samuel Alejandro Garces Florez <sesalus@gmail.com>
2023-11-02 07:22:17 -0500

    Post function INSERT INTO producto SET

--- app.js
+++ app.js
@@ -39,7 +39,6 @@ app.get('/api/producto', (req,res)=>{
   });
 });
 
-
 app.get('/api/producto/:id', (req,res)=>{
   conexion.query('SELECT * FROM producto WHERE id=?', [req.params.id] , (error,fila)=>{
       if(error){
@@ -47,6 +46,18 @@ app.get('/api/producto/:id', (req,res)=>{
       }else{
          res.send(fila);
          res.send(fila[0].descripción);
+      }
+  });
+});
+
+app.post('/api/producto',(req,res)=>{
+  let data = {id:req.body.id, descripcion:req.body.descripcion, precio:req.body.precio, stock:req.body.stock};
+  let sql = "INSERT INTO producto SET ?";
+  conexion.query(sql, data, function(error,results){
+      if(error){
+          throw error;
+      }else{
+         res.send({ message: 'Producto registrado correctamente' });     
       }
   });
 });
